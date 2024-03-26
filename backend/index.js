ba8142e45daf55e1bf2c9c8a6c169a58d7d42db3
ahmed achour <66746240+17med@users.noreply.github.com>
2024-03-26 15:59:38 +0100

    update v0.1.10 test3 state db4

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use(express.json())
 app.use(cookieParser({secert:process.env.secret}))
 app.use(express.static('dist'))
 app.get("/api/dbsatete",(req,res)=>{
-    res.send({"state":db.isconnceted})
+    res.send({"state":db.isconnceted,"db":process.env.secret})
 })
 app.use(MainRoute);
 
